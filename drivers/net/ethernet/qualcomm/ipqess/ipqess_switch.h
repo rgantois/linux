e1544a193d5ef2a61c39b3ee2e95bb5d84f17fb8
Romain Gantois <romain.gantois@bootlin.com>
2023-10-03 09:00:09 +0200

    net: ipqess: introduce the Qualcomm IPQESS driver
    The Qualcomm IPQ4019 Ethernet Switch Subsystem for the IPQ4019 chip
    includes an internal Ethernet switch based on the QCA8K IP.
    
    The CPU-to-switch port data plane depends on the IPQESS EDMA Controller,
    a simple 1G Ethernet controller. It is connected to the switch through an
    internal link, and doesn't expose directly any external interface.
    
    The EDMA controller has 16 RX and TX queues, with a very basic RSS fanout
    configured at init time.
    
    Signed-off-by: Romain Gantois <romain.gantois@bootlin.com>

---
+++ drivers/net/ethernet/qualcomm/ipqess/ipqess_switch.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0 OR ISC */
+
+#ifndef IPQESS_SWITCH_H
+#define IPQESS_SWITCH_H
+
+#include <linux/dsa/qca8k.h>
+
+#define IPQESS_SWITCH_MAX_PORTS       5
+#define IPQESS_SWITCH_AGEING_TIME_MIN 7000
+#define IPQESS_SWITCH_AGEING_TIME_MAX 458745000
+#define IPQESS_SWITCH_CPU_PORT        0
+#define IPQESS_SWITCH_NUM_PORTS       5
+#define IPQESS_SWITCH_ID              0x14
+
+struct ipqess_switch {
+	struct net_device *napi_leader;
+	struct qca8k_priv *priv;
+	struct ipqess_edma *edma;
+	struct ipqess_port *port_list[IPQESS_SWITCH_MAX_PORTS];
+	struct devlink *devlink;
+	struct reset_control *psgmii_rst;
+	bool port0_enabled;
+
+	/* List of MAC addresses that must be forwarded on the cpu port */
+	struct mutex		addr_lists_lock;
+	struct list_head	fdbs;
+	struct list_head	mdbs;
+};
+
+struct ipqess_devlink_priv {
+	struct ipqess_switch *sw;
+};
+
+unsigned int ipqess_switch_fastest_ageing_time(struct ipqess_switch *sw,
+					       unsigned int ageing_time);
+int ipqess_set_ageing_time(struct ipqess_switch *sw, unsigned int msecs);
+
+int ipqess_switch_setup(struct ipqess_switch *sw);
+
+#endif
